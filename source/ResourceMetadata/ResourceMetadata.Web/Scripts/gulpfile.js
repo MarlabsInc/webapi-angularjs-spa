5bf725101bb282ed0680e5fb88067cac99b3b9a8
DileeCD <cd.dileep@yahoo.com>
2014-02-18 17:05:50 +0530

    gulp tasks minifications

--- source/ResourceMetadata/ResourceMetadata.Web/Scripts/gulpfile.js
+++ source/ResourceMetadata/ResourceMetadata.Web/Scripts/gulpfile.js
@@ -5,29 +5,49 @@ var gulp = require('gulp'),
     gutil = require('gulp-util'),
     uglify = require('gulp-uglify'),
     jshint = require('gulp-jshint'),
-    browserify = require('gulp-browserify'),
-    concat = require('gulp-concat')
-jshintreporter = require('jshint-stylish');
+    concat = require('gulp-concat'),
+    jshintreporter = require('jshint-stylish'),
+    minifycss = require('gulp-minify-css'),
+    rename = require('gulp-rename');
 
-gulp.task('js', function () {
-    gulp.src('./app/**/*.js')
-        .pipe(jshint())
-        .pipe(jshint.reporter(jshintreporter))
+var filePath = {
+    appjsminify: { src: './app/**/*.js', dest: './app' },
+    libsjsminify: { src: ['libs/**/*.js', '!*.min.js', '!/**/*.min.js'], dest: 'libs/' },
+    jshint: { src: './app/**/*.js' },
+    minifycss: { src: ['../Content/themes/**/*.css', '!*.min.css', '!/**/*.min.css'], dest: '../Content/themes/' }
+};
+
+
+gulp.task('app-js-minify', function () {
+    gulp.src(filePath.appjsminify.src)
         .pipe(uglify())
         .pipe(concat('ngscripts.js'))
-        .pipe(gulp.dest('./app'));
-});
-gulp.task('default', function () {
-    gulp.run('js');
+        .pipe(gulp.dest(filePath.appjsminify.dest));
 });
 
-gulp.task('browserify', function () {
-    gulp.src('./app/app.js')
-    .pipe(browserify())
-    .pipe(gulp.dest('./app/browserify'))
+gulp.task('libs-js-minify', function () {
+    gulp.src(filePath.libsjsminify.src)
+    .pipe(uglify())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(filePath.libsjsminify.dest));
 });
 
 
+gulp.task('jshint', function () {
+    gulp.src(filePath.jshint.src)
+      .pipe(jshint())
+      .pipe(jshint.reporter(jshintreporter));
+});
 
+
+gulp.task('minify-css', function () {
+    /*Excludes already minified files.*/
+    gulp.src(filePath.minifycss.src)
+    .pipe(minifycss())
+    .pipe(rename({ suffix: '.min' }))
+    .pipe(gulp.dest(filePath.minifycss.dest));
+});
+
+gulp.task('default', ['app-js-minify', 'libs-js-minify', 'minify-css']);
 
 //gulp.watch('./app/**/*.js', ['js']);
